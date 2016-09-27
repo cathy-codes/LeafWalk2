5d4fee67781b06bc361e7d7012b4f3e45a3039f2
Cathy <cathy.chi.chan@gmail.com>
2016-09-27 21:57:54 +0100

    Use local storage to save login details

--- leafwalk2/www/js/services.js
+++ leafwalk2/www/js/services.js
@@ -3,7 +3,7 @@
 angular.module('leafWalk.services', ['ngResource'])
         .constant("baseURL","http://localhost:3000/")
         .service('openSpacesFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-          
+
           return $resource(baseURL + "openspaces/:id", null, {
             'update': {
                 method: 'PUT'
@@ -42,5 +42,22 @@ angular.module('leafWalk.services', ['ngResource'])
           };
 
           return favFac;
+        }])
+
+        .factory('$localStorage', ['$window', function($window) {
+          return {
+            store: function(key, value) {
+              $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+              return $window.localStorage[key] || defaultValue;
+            },
+            storeObject: function(key, value) {
+              $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key,defaultValue) {
+              return JSON.parse($window.localStorage[key] || defaultValue);
+            }
+          }
         }])
 ;
